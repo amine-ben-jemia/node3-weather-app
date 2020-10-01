0312f89822af02e026dbb3a8c874bf8e01b0fd92
unknown <amine.ben.jemia@gmail.com>
2020-10-01 13:09:00 +0100

    Remove unnecessary console.log call

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-const { error } = require('console')
+
 
 const app = express()
 //Definir le chemain des repertoires 
